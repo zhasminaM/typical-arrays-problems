9a033e9bc3a04952376d5ad0bac582dd7f568d4b
Zhasmina <jassmina_121@mail.ru>
2020-09-30 00:09:58 +0300

    dosc: update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+    let min = array[0];
+    for (var i = 0; i < array.length; i++) {
+        if (min > array[i]) min = array[i];
+    }
+    return min;
 }
 
+ 
+
+
 exports.max = function max (array) {
-  return 0;
+    let max = array[0]; 
+    for (var i = 0; i < array.length; i++) { 
+        if (max < array[i]) max = array[i]; 
+    }
+
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    return avg.reduce((a, b) => (a + b)) / avg.length;
 }
